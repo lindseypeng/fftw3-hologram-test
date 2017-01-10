b3dd3a74002e8bb6a1fe93394db16bbd3b992911
lindseypeng <lindsey.peng@mail.utoronto.ca>
2017-01-10 13:42:01 -0500

    Update complexnumber.h

--- complexnumber.h
+++ complexnumber.h
@@ -1,7 +1,7 @@
 #ifndef COMPLEXNUMBER_H_INCLUDED
 #define COMPLEXNUMBER_H_INCLUDED
 
-int complexnumber(int row, int column);
+int complexnumber(int step1,int width1, int height1);
 
 
 #endif // COMPLEXNUMBER_H_INCLUDED
