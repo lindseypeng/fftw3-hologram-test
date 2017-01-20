2b2944f5bdf92b71aecf0144e383b1eaf8af2859
lindseypeng <lindsey.peng@mail.utoronto.ca>
2017-01-20 09:41:12 -0500

    Update main.cpp
    cleaned

--- main.cpp
+++ main.cpp
@@ -18,17 +18,13 @@ using namespace std;
 int main()
 {
     cv::Mat img1;
-    cv::Mat img2;
     cv::Mat img3;
-    cv::Mat img4;
+
     uchar *img1_data;
-    uchar *img2_data;
     uchar *img3_data;
-    uchar *img4_data;
 
     fftw_complex    *data_in;
     fftw_complex    *fft;
-    fftw_complex    *ifft;
     fftw_complex    *transfer;
     fftw_complex    *products;
     fftw_complex    *inverse;
@@ -37,14 +33,11 @@ int main()
     fftw_complex    *gg;
 
     fftw_plan       plan_f;
-    fftw_plan       plan_b;
     fftw_plan       plan_g;
     fftw_plan       plan_I;
 
-
     int             width, height, step;
     int             i, j, k;
-
 
     const complex<double> J (0.0,1.0);
     const complex<double> d (130.0,0.0);
@@ -59,41 +52,30 @@ int main()
 
     double *XX =new double[1024*1280] ;
     double *YY = new double[1024*1280] ;
-
     double *final2=new double[1024*1280] ;
     // load original image
- img1= imread("/home/alinsi/codeblocks/fft2/microbead1.tif", CV_LOAD_IMAGE_GRAYSCALE);
+    img1= imread("/home/alinsi/codeblocks/fft2/microbead1.tif", CV_LOAD_IMAGE_GRAYSCALE);
 
     // create new image for IFFT result
-    img2 = img1.clone();
     img3 = img1.clone();
-    img4 = img4.clone();
 
     // get image properties
     width  	  = img1.size().width;
     height 	  = img1.size().height;
     step	  = img1.step;
     img1_data = ( uchar* ) img1.data;
-    img2_data = ( uchar* ) img2.data;
     img3_data = ( uchar* ) img3.data;
-    img4_data = ( uchar* ) img4.data;
+
     // initialize arrays for fftw operations
     data_in = fftw_alloc_complex(width * height);
     fft     = fftw_alloc_complex(width * height);
-    ifft    = fftw_alloc_complex(width * height);
     transfer = fftw_alloc_complex(width * height);
     products =fftw_alloc_complex(width * height);
     inverse = fftw_alloc_complex(width * height);
     gg = fftw_alloc_complex(width*height);
 
-
-
-
     // create plans
     plan_f = fftw_plan_dft_2d( height, width, data_in, fft,  FFTW_FORWARD,  FFTW_ESTIMATE );
-
-
-
 
     // load img1's data to fftw input
     for( i = 0, k = 0 ; i < height ; i++ ) {
@@ -104,14 +86,6 @@ int main()
         }
     }
 
-
-
-for( i = 0; i < 2 ; i++ ) {
-		for( j = 0 ; j < 5 ; j++ ) {
-			double h =img1_data[i*step+j];
-			cout<<"original image at column"<<i <<" and row "<< j <<" is "<<h<<endl;
-		}
-	}
     ////calculating transfer function//
 //for YY row major loop
     for (int j=-height/2,k=0;j<height/2;j++){
@@ -121,15 +95,12 @@ for( i = 0; i < 2 ; i++ ) {
         }
            }
 //for XX row major loop
-
     for (int j=-height/2,k=0;j<height/2;j++){
         for (int i=-width/2;i<width/2;i++){
         XX[k]=(i)*dx;
         k++;
         }
     }
-
-
 
 //creating row major array of transfer function g
     for (int j=0;j<height*width;j++){
@@ -144,17 +115,11 @@ memcpy( &gg, &g, sizeof (fftw_complex));
  // perform FFT
     fftw_execute( plan_f );
 
-
-
-
-//create another plan
-
 //plan_g = fftw_plan_dft_2d( width, height, reinterpret_cast<fftw_complex*>(g), transfer,FFTW_FORWARD, FFTW_ESTIMATE);
 plan_g = fftw_plan_dft_2d( height, width, gg, transfer,FFTW_FORWARD, FFTW_ESTIMATE);
-plan_b = fftw_plan_dft_2d( height, width, transfer,     ifft, FFTW_BACKWARD, FFTW_ESTIMATE );
+
     //perform another FFT
     fftw_execute( plan_g );
-
 
 //now multiple FFT1  FFT2/////////double check multiplication of copmlex numbers(xu-yv)+(xv+yu)i
      for( i=0,k = 0 ; i < (height*width); i++ ) {
@@ -164,14 +129,10 @@ plan_b = fftw_plan_dft_2d( height, width, transfer,     ifft, FFTW_BACKWARD, FFT
             k++;
         }
 
-
 //create another plan
 plan_I = fftw_plan_dft_2d( height, width, products,inverse,FFTW_BACKWARD, FFTW_ESTIMATE);
 
-
-
     fftw_execute( plan_I );
-
     //    // normalize final inverse result
     for( i = 0 ; i < ( width * height ) ; i++ ) {
         inverse[i][0] /= (( double )( width * height ));
@@ -179,52 +140,27 @@ plan_I = fftw_plan_dft_2d( height, width, products,inverse,FFTW_BACKWARD, FFTW_E
 
     }
 
-
-
-    // perform IFFT
-    fftw_execute( plan_b );
-
-
-
-    // normalize IFFT result
+//obtain magnitude of inverse
     for( i = 0 ; i < ( width * height ) ; i++ ) {
-        ifft[i][0] /= (( double )( width * height ));
-        ifft[i][1] /= (( double )( width * height ));
+    final2[i]=sqrt(inverse[i][0]*inverse[i][0]+inverse[i][1]*inverse[i][1]);
 
     }
+//otain maximum value from inverse array
+    double max2=final2[0];
+    double min2=final2[0];
 
-
-//obtain magnitude of inverse
-for( i = 0 ; i < ( width * height ) ; i++ ) {
-final2[i]=sqrt(inverse[i][0]*inverse[i][0]+inverse[i][1]*inverse[i][1]);
-
-}
-
-double max2=final2[0];
-double min2=final2[0];
-
-for( i = 0; i < ( width * height ) ; i++ ) {
-    if (final2[i]>max2)
-        max2=final2[i];
-    if (final2[i]<min2)
-        min2=final2[i];
-}
-
-
-for (i=0,k=0;i<width*height;i++){
-    final2[k] /= max2;
-    final2[k] *= 255;
-k++;
-}
-
-
-
-    // copy IFFT result to img2's data
-    for( i = 0, k = 0 ; i < height ; i++ ) {
-		for( j = 0 ; j < width ; j++ ) {
-			img2_data[i * step + j] = ( uchar )(ifft[k++][0]);
-		}
-	}
+    for( i = 0; i < ( width * height ) ; i++ ) {
+        if (final2[i]>max2)
+            max2=final2[i];
+        if (final2[i]<min2)
+            min2=final2[i];
+    }
+//normalize final2 into range of 0 and 255
+    for (i=0,k=0;i<width*height;i++){
+        final2[k] /= max2;
+        final2[k] *= 255;
+        k++;
+        }
 	    //copy  transfer data into img3
     for( i = 0, k = 0 ; i < height ; i++ ) {
 		for( j = 0 ; j < width ; j++ ) {
@@ -232,27 +168,21 @@ k++;
 		}
 	}
 
-
+// rearrange the quadrants of Fourier image
+// so that the origin is at the image center
 int cx = width/2;
 int cy = height/2;
 
-
-// rearrange the quadrants of Fourier image
-// so that the origin is at the image center
 Mat tmp;
 Mat q0(img3, Rect(0, 0, cx, cy));
-
 Mat q1(img3, Rect(cx, 0, cx, cy));
-
 Mat q2(img3, Rect(0, cy, cx, cy));
 Mat q3(img3, Rect(cx, cy, cx, cy));
-
 q0.copyTo(tmp);
 q3.copyTo(q0);
 tmp.copyTo(q3);
 
 Mat tmp1;
-
 q1.copyTo(tmp1);
 q2.copyTo(q1);
 tmp1.copyTo(q2);
@@ -262,11 +192,9 @@ tmp1.copyTo(q2);
 //
     // display images
     cv::namedWindow( "original_image", CV_WINDOW_NORMAL );
-    cv::namedWindow( "Transfer function", CV_WINDOW_NORMAL );
     cv::namedWindow( "Inverse", CV_WINDOW_NORMAL );
     //cv::namedWindow("transfer",CV_WINDOW_AUTOSIZE);
     cv::imshow( "original_image", img1 );
-    cv::imshow( "Transfer function", img2 );
     cv::imshow( "Inverse", img3 );
   //  cv::imshow("transfer",img4);
     char key;
@@ -291,18 +219,14 @@ tmp1.copyTo(q2);
    //delete [] temporary;
 
     img1.release();
-    img2.release();
     img3.release();
-    img4.release();
+
     fftw_destroy_plan( plan_f );
-    fftw_destroy_plan( plan_b );
     fftw_destroy_plan( plan_I );
     fftw_destroy_plan( plan_g );
 
     fftw_free( data_in );
     fftw_free( fft );
-    fftw_free( ifft );
-
     fftw_free( products );
     fftw_free( inverse);
     fftw_free(transfer);
